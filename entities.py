e874fe12d79a1e54fb4b3358a328beb312a12eb1
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-09 19:03:57 +0100

    Update entities.py

--- entities.py
+++ entities.py
@@ -1,6 +1,15 @@
+entitylist = []
+
 class Entity():
-    def __init__(self, name: str, x: int, y: int):
+    def __init__(self, name, x, y, char):
         self.name = name
         self.x = x
         self.y = y
+        self.char = char
         self.speed = 16
+        
+def create_entity(name: str, x: int, y: int, char: str):
+    global entitylist
+    newEntity = Entity(name, x, y, char)
+    entitylist.append(newEntity)
+    return newEntity
