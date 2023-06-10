ea90f55c36c9df33db1d0f8ed305f0cc0a69d6ce
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-10 19:58:15 +0100

    Update colours.py

--- colours.py
+++ colours.py
@@ -1,7 +1,9 @@
 colours = {
     "white": (255,255,255),
     "black": (0,0,0),
-    "green": (0,255,0)
+    "green": (0,255,0),
+    "mint": (175, 225, 175),
+    "camel": (193, 154, 107)
 }
 
 def getcolours():
