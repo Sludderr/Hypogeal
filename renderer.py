51a921e193fb924f61e7d0bc86521fc87f08bee3
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-10 16:27:12 +0100

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1,15 +1,29 @@
 import pygame
 import entities
+import gamemap
 
-def update(screen, font):
+black = (0,0,0)
+
+
+def update(screen, font, width, height):
     screen.fill((0,0,0))
+    
+    Map = gamemap.getmap()
+    for y in range(height):
+        for x in range(width):
+            CurrentTile = Map[y][x]
+            if CurrentTile.visible == True:
+                CurrentTilePos = (CurrentTile.x * 16, CurrentTile.y * 16)
+                text = font.render(CurrentTile.char, True, CurrentTile.colour)
+                screen.blit(text, CurrentTilePos)
+            
     
     entitylist = entities.entitylist
 
     for i in range(len(entitylist)):
         CurrentEntity = entitylist[i]
         CurrentEntityPos = (CurrentEntity.x * 16, CurrentEntity.y * 16)
-        text = font.render(CurrentEntity.char, True, CurrentEntity.colour)
+        text = font.render(CurrentEntity.char, True, CurrentEntity.colour, black)
         screen.blit(text, CurrentEntityPos)
         
     pygame.display.update()
