f59e9182fd39192f8e6fa436a9d946d401629695
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-10 14:47:36 +0100

    decomposinating

--- main.py
+++ main.py
@@ -1,74 +1,41 @@
 import pygame
-import sys
 import entities
+import renderer
+import input_handler
 
-width = 1024
-height = 896
+width = 64
+height = 56
+
+screenwidth = width * 16
+screenheight = height * 16
 
 white = (255, 255, 255)
 black = (0, 0, 0)
+green = (0, 255, 0)
 
 pygame.init()
 pygame.font.init()
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((screenwidth, screenheight))
 clock = pygame.time.Clock()
 
-pygame.display.flip()
 pygame.display.set_caption('Test')
 font = pygame.font.SysFont("timesnewroman", 16)
 pygame.display.flip()
 
-player = entities.create_entity("Player", 0, 0, "@")
-dummy = entities.create_entity("Dummy", 64, 64, "#")
+player = entities.create_entity("Player", 0, 0, "@", white)
+dummy = entities.create_entity("Dummy", 4, 4, "#", green)
 
 running = True
 
 while running:
-
     pygame.time.delay(100)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_KP8]:
-        player.y -= player.speed
-        print("UP")
-    if keys[pygame.K_KP2]:
-        player.y += player.speed
-        print("DOWN")
-    if keys[pygame.K_KP4]:
-        player.x -= player.speed
-        print("LEFT")
-    if keys[pygame.K_KP6]:
-        player.x += player.speed
-        print("RIGHT")
-    if keys[pygame.K_KP7]:
-        player.y -= player.speed
-        player.x -= player.speed
-        print("UP_LEFT")
-    if keys[pygame.K_KP9]:
-        player.y -= player.speed
-        player.x += player.speed
-        print("UP_RIGHT")
-    if keys[pygame.K_KP1]:
-        player.y += player.speed
-        player.x -= player.speed
-        print("DOWN_LEFT")
-    if keys[pygame.K_KP3]:
-        player.y += player.speed
-        player.x += player.speed
-        print("DOWN_RIGHT")
+            running = False
     
-    screen.fill((0,0,0))
+    input_handler.handle(player)
     
-    entitylist = entities.entitylist
-
-    for i in range(len(entitylist)):
-        CurrentEntityPos = (entitylist[i].x, entitylist[i].y)
-        text = font.render(entitylist[i].char, True, white)
-        screen.blit(text, CurrentEntityPos)
-        
-    pygame.display.update()
+    renderer.update(screen, font)
+    
 pygame.quit()
