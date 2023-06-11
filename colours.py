7fe089d395ef3667108ac8edbd90b1d583c82f9b
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-11 22:22:29 +0100

    Update colours.py

--- colours.py
+++ colours.py
@@ -3,8 +3,9 @@ colours = {
     "black": (0,0,0),
     "green": (0,255,0),
     "mint": (175, 225, 175),
-    "camel": (193, 154, 107)
+    "camel": (193, 154, 107),
+    "slate": (112, 128, 144),
+    "darkslate": (47, 79, 79) 
 }
-
 def getcolours():
     return colours
