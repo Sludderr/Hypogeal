d4074bc5cdc87d8cf41d3505ad0e688175e0f4ff
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-11 01:09:40 +0100

    Update input_handler.py

--- input_handler.py
+++ input_handler.py
@@ -12,53 +12,61 @@ def handle(player):
     if keys[pygame.K_KP8]:
         if detectcollision(player.x, player.y-1, Map) != True:
             player.y -= player.speed
-            print("UP")
+            # UP
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP2]:
         if detectcollision(player.x, player.y+1, Map) != True:
             player.y += player.speed
-            print("DOWN")
+            # DOWN
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP4]:
         if detectcollision(player.x-1, player.y, Map) != True:
             player.x -= player.speed
-            print("LEFT")
+            # LEFT
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP6]:
         if detectcollision(player.x+1, player.y, Map) != True:
             player.x += player.speed
-            print("RIGHT")
+            # RIGHT
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP7]:
         if detectcollision(player.x-1, player.y-1, Map) != True:
             player.y -= player.speed
             player.x -= player.speed
-            print("UP_LEFT")
+            # UP_LEFT
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP9]:
         if detectcollision(player.x+1, player.y-1, Map) != True:
             player.y -= player.speed
             player.x += player.speed
-            print("UP_RIGHT")
+            # UP_RIGHT
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP1]:
         if detectcollision(player.x-1, player.y+1, Map) != True:
             player.y += player.speed
             player.x -= player.speed
-            print("DOWN_LEFT")
+            # DOWN_LEFT
         else:
+            # COLLISION
             print("The way is blocked")
     elif keys[pygame.K_KP3]:
         if detectcollision(player.x+1, player.y+1, Map) != True:
             player.y += player.speed
             player.x += player.speed
-            print("DOWN_RIGHT")
+            # DOWN_RIGHT
         else:
+            # COLLISION
             print("The way is blocked")
         
