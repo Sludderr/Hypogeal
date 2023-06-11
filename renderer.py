988813231a374232e2952fa39f1e5ff76f1422da
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-11 22:24:31 +0100

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -2,6 +2,8 @@ import pygame
 import entities
 import gamemap
 import colours
+import raycaster
+
 
 colourdict = colours.getcolours() 
 
@@ -9,19 +11,22 @@ black = colourdict["black"]
 
 def update(screen, font, width, height):
     screen.fill(black)
+    player = entities.getplayer()
+    entitylist = entities.entitylist
     
     Map = gamemap.getmap()
     for y in range(height):
         for x in range(width):
             CurrentTile = Map[y][x]
-            if CurrentTile.visible == True:
+            if CurrentTile.name == "border":
                 CurrentTilePos = (CurrentTile.x * 16, CurrentTile.y * 16)
                 text = font.render(CurrentTile.char, True, CurrentTile.colour)
                 screen.blit(text, CurrentTilePos)
-            
+            elif CurrentTile.rendered == True:
+                CurrentTilePos = (CurrentTile.x * 16, CurrentTile.y * 16)
+                text = font.render(CurrentTile.char, True, CurrentTile.colour)
+                screen.blit(text, CurrentTilePos)
     
-    entitylist = entities.entitylist
-
     for i in range(len(entitylist)):
         CurrentEntity = entitylist[i]
         CurrentEntityPos = (CurrentEntity.x * 16, CurrentEntity.y * 16)
@@ -29,4 +34,3 @@ def update(screen, font, width, height):
         screen.blit(text, CurrentEntityPos)
         
     pygame.display.update()
-
