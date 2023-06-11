c5596d3cb6c0099bf7f7e35dfe41bb62fe6ee980
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-11 22:25:42 +0100

    Create state.py AAAAAAAAAAAAAAAAAAA

---
+++ state.py
@@ -0,0 +1,19 @@
+import raycaster
+import procgeneration
+
+def update_visibility(Map, playerx, playery, width, height):
+    for y in range(21):
+        for x in range(21):
+            ytemp = playery-10+y
+            xtemp = playerx-10+x
+            if ytemp > 0 and ytemp < height and xtemp > 0 and xtemp < width:
+                if raycaster.distance(playerx, playery, xtemp, ytemp) < 10:
+                    if procgeneration.checkadjacent(Map, width, height, xtemp, ytemp, "wall", "border") == 8:
+                        Map[ytemp][xtemp].rendered = False
+                    elif raycaster.raydetect(Map, playerx, playery, xtemp, ytemp, 0, True, 0, 0) == True:
+                        Map[ytemp][xtemp].rendered = True
+                    else:
+                        Map[ytemp][xtemp].rendered = False
+                else:
+                    Map[ytemp][xtemp].rendered = False
+    return Map
