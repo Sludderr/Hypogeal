f695d939dfaab4b4352447679f870e7f86ad9409
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-12 21:30:06 +0100

    Update raycaster.py

--- raycaster.py
+++ raycaster.py
@@ -10,11 +10,11 @@ def distance(playerx, playery, tilex, tiley):
 
 def raydetect(Map, ax, ay, bx, by, grad, firstpass, dx, dy):
     if firstpass == False:
-        tempy = ay - 2
-        tempx = ax - 2
+        tempy = int(ay) - 2
+        tempx = int(ax) - 2
     else:
-        tempy = ay - 1
-        tempx = ax - 1
+        tempy = int(ay) - 1
+        tempx = int(ax) - 1
     if Map[tempy][tempx].name == "wall":
         return False
     if tempx == bx:
@@ -47,13 +47,52 @@ def raydetect(Map, ax, ay, bx, by, grad, firstpass, dx, dy):
         if abs(dx) < abs(dy):
             dx += math.sqrt((grad**2) + 1)
             if ax < bx:
-                return raydetect(Map, int(ax+1), int(ay+grad), bx, by, grad, False, dx, dy)
+                return raydetect(Map, ax+1, ay+grad, bx, by, grad, False, dx, dy)
             else:
-                return raydetect(Map, int(ax-1), int(ay+grad), bx, by, grad, False, dx, dy)
+                return raydetect(Map, ax-1, ay+grad, bx, by, grad, False, dx, dy)
         else:
             dy += math.sqrt(((1 / grad)**2) + 1)
             if ay < by:
-                return raydetect(Map, int(ax+(ay/grad)), int(ay+1), bx, by, grad, False, dx, dy)
+                return raydetect(Map, ax+(ay/grad), ay+1, bx, by, grad, False, dx, dy)
             else:
-                return raydetect(Map, int(ax+(ay/grad)), int(ay-1), bx, by, grad, False, dx, dy)
+                return raydetect(Map, ax+(ay/grad), ay-1, bx, by, grad, False, dx, dy)
     return True
+
+def pathdetect(Map, startx, starty, destx, desty):
+    while (startx != destx or starty != desty) and Map[starty][startx].name != "wall":
+        ndist = distance(startx, starty-1, destx, desty)
+        nedist = distance(startx+1, starty-1, destx, desty)
+        edist = distance(startx+1, starty, destx, desty)
+        sedist = distance(startx+1, starty+1, destx, desty)
+        sdist = distance(startx, starty+1, destx, desty)
+        swdist = distance(startx-1, starty+1, destx, desty)
+        wdist = distance(startx-1, starty, destx, desty)
+        nwdist = distance(startx-1, starty-1, destx, desty)
+        
+        if ndist < nedist and ndist < edist and ndist < sedist and ndist < sdist and ndist < swdist and ndist < wdist and ndist < nwdist:
+            starty -= 1
+        elif nedist < edist and nedist < sedist and nedist < sdist and nedist < swdist and nedist < wdist and nedist < nwdist:
+            starty -= 1
+            startx += 1
+        elif edist < sedist and edist < sdist and edist < swdist and edist < wdist and edist < nwdist:
+            startx += 1
+        elif sedist < sdist and sedist < swdist and sedist < wdist and sedist < nwdist:
+            starty += 1
+            startx += 1
+        elif sdist < swdist and sdist < wdist and sdist < nwdist:
+            starty += 1
+        elif swdist < wdist and swdist < nwdist:
+            starty += 1
+            startx -= 1
+        elif wdist < nwdist:
+            startx -= 1
+        else:
+            starty -= 1
+            startx -= 1
+            
+    if startx != destx or starty != desty:
+        print("startx = ", startx, " starty = ", starty, " destx = ", destx, " desty = ", desty)
+        return False
+    else:
+        
+        return True
