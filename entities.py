916eb3191f4473b3743928ea971fb3f3da9a9829
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-06-14 20:59:34 +0100

    Update entities.py

--- entities.py
+++ entities.py
@@ -1,17 +1,18 @@
 entitylist = []
 
 class Entity():
-    def __init__(self, name, x, y, char, colour):
+    def __init__(self, name, x, y, char, colour, viewrestrict):
         self.name = name
         self.x = x
         self.y = y
         self.char = char
         self.colour = colour
+        self.viewrestrict = viewrestrict
         self.speed = 1
         
-def create_entity(name: str, x: int, y: int, char: str, colour):
+def create_entity(name: str, x: int, y: int, char: str, colour, viewrestrict):
     global entitylist
-    newEntity = Entity(name, x, y, char, colour)
+    newEntity = Entity(name, x, y, char, colour, viewrestrict)
     entitylist.insert(0,newEntity)
     return newEntity
 
