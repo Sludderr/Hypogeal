de2811dcc3c3f6a8db9e76505301b26d031765a0
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-07-21 18:56:32 +0100

    Update raycaster.py

--- raycaster.py
+++ raycaster.py
@@ -1,12 +1,12 @@
 import math
 
+
+# Use pythagoras to get distance
 def distance(playerx, playery, tilex, tiley):
     xdist = playerx - tilex
     ydist = playery - tiley
     answer = math.sqrt((xdist ** 2) + (ydist ** 2))
     return answer
-
-
 
 def raydetect(Map, ax, ay, bx, by, grad, firstpass, dx, dy):
     if firstpass == False:
@@ -59,7 +59,9 @@ def raydetect(Map, ax, ay, bx, by, grad, firstpass, dx, dy):
     return True
 
 def pathdetect(Map, startx, starty, destx, desty):
+    # While the player tile hasn't been reached and the current tile isn't a wall (collision)
     while (startx != destx or starty != desty) and Map[starty][startx].name != "wall":
+        # get the distance to the playertile from each surrounding tile
         ndist = distance(startx, starty-1, destx, desty)
         nedist = distance(startx+1, starty-1, destx, desty)
         edist = distance(startx+1, starty, destx, desty)
@@ -68,27 +70,36 @@ def pathdetect(Map, startx, starty, destx, desty):
         swdist = distance(startx-1, starty+1, destx, desty)
         wdist = distance(startx-1, starty, destx, desty)
         nwdist = distance(startx-1, starty-1, destx, desty)
-        
+
+        # If North is closest, go North
         if ndist < nedist and ndist < edist and ndist < sedist and ndist < sdist and ndist < swdist and ndist < wdist and ndist < nwdist:
             starty -= 1
+        # If North-East is closest, go North-East
         elif nedist < edist and nedist < sedist and nedist < sdist and nedist < swdist and nedist < wdist and nedist < nwdist:
             starty -= 1
             startx += 1
+        # If East is closest, go East
         elif edist < sedist and edist < sdist and edist < swdist and edist < wdist and edist < nwdist:
             startx += 1
+        # If South-East is closest, go South-East
         elif sedist < sdist and sedist < swdist and sedist < wdist and sedist < nwdist:
             starty += 1
             startx += 1
+        # If South is closest, go South
         elif sdist < swdist and sdist < wdist and sdist < nwdist:
             starty += 1
+        # If South-West is closest, go South-West
         elif swdist < wdist and swdist < nwdist:
             starty += 1
             startx -= 1
+        # If West is closest, go West
         elif wdist < nwdist:
             startx -= 1
+        # If North-West is closes, go West
         else:
             starty -= 1
             startx -= 1
+    
             
     if startx != destx or starty != desty:
         return False
