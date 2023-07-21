813c0f94194c783808631b188c8cd7b9210622ec
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-07-21 18:56:56 +0100

    Update state.py

--- state.py
+++ state.py
@@ -2,18 +2,28 @@ import raycaster
 import procgeneration
 
 def update_visibility(Map, playerx, playery, width, height):
+    # iterate through map tiles within the range of the player
     for y in range(21):
         for x in range(21):
+            # set first position in map
             ytemp = playery-10+y
             xtemp = playerx-10+x
             if ytemp > 0 and ytemp < height and xtemp > 0 and xtemp < width:
+                # if inside the map
                 if raycaster.distance(playerx, playery, xtemp, ytemp) < 10:
+                    # if within viewing distance
                     if procgeneration.checkadjacent(Map, width, height, xtemp, ytemp, "wall", "border") == 8 or raycaster.pathdetect(Map, playerx, playery, xtemp, ytemp) == False:
+                        # if obscured or inside a wall
                         Map[ytemp][xtemp].rendered = False
-                    #elif raycaster.raydetect(Map, playerx, playery, xtemp, ytemp, 0, True, 0, 0) == True:
-                        #Map[ytemp][xtemp].rendered = True
+                    
                     else:
+                        # otherwise render it
                         Map[ytemp][xtemp].rendered = True
                 else:
                     Map[ytemp][xtemp].rendered = False
     return Map
+
+
+#elif raycaster.raydetect(Map, playerx, playery, xtemp, ytemp, 0, True, 0, 0) == True:
+                        #Map[ytemp][xtemp].rendered = True:
+
