fe4c6cd5a36f7d160dbe1fa9fd8f102c56343c44
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-09-11 14:25:28 +0100

    Update statecopy.py

--- statecopy.py
+++ statecopy.py
@@ -1,18 +1,18 @@
-def update_visibility(Map, playerx, playery, width, height):
+def update_visibility(Map, playerx, playery):
     # iterate through map tiles within the range of the player
     for y in range(21):
         for x in range(21):
             # set first position in map
+            # will only check the 20 diameter circle around the player
             ytemp = playery-10+y
             xtemp = playerx-10+x
-            if ytemp > 0 and ytemp < height and xtemp > 0 and xtemp < width:
+            if insidemap(xtemp, ytempt):
                 # if inside the map
-                if raycaster.distance(playerx, playery, xtemp, ytemp) < 10:
+                if distance(xtemp, ytemp) < 10:
                     # if within viewing distance
-                    if procgeneration.checkadjacent(Map, width, height, xtemp, ytemp, "wall", "border") == 8 or raycaster.pathdetect(Map, playerx, playery, xtemp, ytemp) == False:
+                    if checkadjacent() == 8 or pathdetect(Map, xtemp, ytemp) == False:
                         # if obscured or inside a wall
                         Map[ytemp][xtemp].rendered = False
-                    
                     else:
                         # otherwise render it
                         Map[ytemp][xtemp].rendered = True
