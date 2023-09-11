da1b66f56e7b7d48d92a4e9bbd544614e437bde2
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-09-11 14:33:47 +0100

    Create gamemapcopy.py

---
+++ gamemapcopy.py
@@ -0,0 +1,24 @@
+def setup(widther, heighter, startx, starty):
+    global Map
+    global width
+    global height
+
+    # Initialise 2D array
+    Map = NewArray(width,height,floor("floor",x,y))
+
+    # Procedural Generation Algorithm
+    Map = drunkardwalk(Map, width, height)
+
+    # Populate with entities
+    Map = populate(Map, width, height)
+
+    # Set the border to '#'s
+    for x in range(width):
+        Map[0][x] = wall("border",x,0)
+        Map[height-1][x] = wall("border",x,height-1)
+    for y in range(width):
+        Map[y][0] = wall("border",0,y)
+        Map[y][width-1] = wall("border",width-1,y)
+    
+    return Map
+  
