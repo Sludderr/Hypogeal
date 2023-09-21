7971171594d1f3f5e636002f5bdaf11f2961ca0a
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-09-21 14:36:27 +0100

    Update drunkcopy.py

--- drunkcopy.py
+++ drunkcopy.py
@@ -1,35 +1,30 @@
 def drunkardwalk(Map, width, height):
-    # Iterate through each tile of the Map
+    # Iterate through each tile of the Map and set every tile to a wall
     for y in range(height):
         for x in range(width):
             Map[y][x] = wall("wall",x,y)
-            # Set every tile to a wall
-    # Number of tiles placed
-    total = 0
+    total = 0 # Number of tiles placed
     limit = random.randint(700,1300)
-    # Random border width
-    border = random.randint(3, 8)
+    border = random.randint(3, 8) # Random border width
     # Until the number of tiles placed meets the random total (creates variety in the types of map)
     while total <= limit:
+        # tunnel out a floor tile
         if Map[drunky][drunkx].name == "wall":
             Map[drunky][drunkx] = floor("floor",drunkx,drunky)
             total += 1
         # Choose a random direction
         rand = random.randint(1,4)
         if rand == 1:
-            # Go North
-            drunky -= 1
+            drunky -= 1  # Go North
         if rand == 2:
-            # Go East
-            drunkx += 1
+            drunkx += 1  # Go East
         if rand == 3:
-            # Go South
-            drunky += 1
+            drunky += 1  # Go South
         if rand == 4:
-            # Go West
-            drunkx -= 1
+            drunkx -= 1  # Go West
     for y in range(height):
         for x in range(width):
+            # number of surrounding wall tiles
             surrounding = checkadjacent(Map, width, height)
             if surrounding <= 1:
                 # Cull small groups of tiles
