cc1b43feeef7c0953a444d667b9c9385be9e4e87
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-09-24 18:33:34 +0100

    Update procgeneration.py

--- procgeneration.py
+++ procgeneration.py
@@ -2,6 +2,7 @@ import gamemap
 import colours
 import random
 import entities
+import math
 
 colourdict = colours.getcolours() 
 
@@ -43,19 +44,22 @@ def checkadjacent(Map, width, height, x, y, nametag, altnametag):
     
     return count
     
-
+def choosecolour():
+    colourlist = [colourdict["maroon"],colourdict["mint"],colourdict["slate"],colourdict["gray"],colourdict["sandybrown"],colourdict["goldenrod"],colourdict["rosybrown"],colourdict["chocolate"],colourdict["pink"],colourdict["paleviolet"],colourdict["salmon"],colourdict["purple"],colourdict["indigo"],colourdict["mediumpurple"],colourdict["darkaquamarine"],colourdict["teal"],colourdict["crimson"],colourdict["olive"]]
+    return colourlist[random.randint(0,len(colourlist)-1)]
 
 def drunkardwalk(Map, width, height, startx, starty):
     # Iterate through each tile of the Map
+    colour = choosecolour()
     for y in range(height):
         for x in range(width):
-            Map[y][x] = gamemap.Tile_Wall("wall",x,y,(random.randint(80,120), random.randint(0,30), random.randint(25,55)))
+            Map[y][x] = gamemap.Tile_Wall("wall",x,y,(random.randint(max(colour[0]-20,0),min(colour[0]+20,255)), random.randint(max(colour[1]-20,0),min(colour[1]+20,255)), random.randint(max(colour[2]-20,0),min(colour[2]+20,255))))
             # Set every tile to a wall
     drunkx = startx
     drunky = starty
     # Number of tiles placed
     total = 0
-    limit = random.randint(700,1300)
+    limit = random.randint(400,1000)
     # Random border width
     border = random.randint(3, 8)
     # Until the number of tiles placed meets the random total (creates variety in the types of map)
@@ -96,14 +100,35 @@ def drunkardwalk(Map, width, height, startx, starty):
 
 
 def populate(Map, width, height, startx,starty):
+    curx = 0
+    cury = 0
+    while Map[cury][curx].walkable != True or distance(curx,cury,startx,starty) < 10:
+        curx = random.randint(3,width-3)
+        cury = random.randint(3,height-3)
+    stairs = gamemap.Tile("stairs",curx,cury,"Z",mint,True,True,False)
+    Map[cury][curx] = stairs
+    
+    while Map[cury][curx].walkable != True or distance(curx,cury,startx,starty) < 10 or Map[cury][curx].name == "stairs":
+        curx = random.randint(3,width-3)
+        cury = random.randint(3,height-3)
+    newpick = entities.create_entity("pickaxe",curx,cury, 5, "⸕", white)
+    Map[cury][curx].occupants.append(newpick)
+    newpick.pickupable = True
+    
     total = 0
-    limit = random.randint(10, 20)
+    limit = random.randint(3, 13)
     while total < limit:
         curx = random.randint(3,width-3)
         cury = random.randint(3,height-3)
-        if Map[cury][cury].walkable == True:
+        if Map[cury][curx].walkable == True and Map[cury][curx].name != "stairs" and distance(curx, cury, startx,starty) > 3:
             total += 1
-            Map[cury][curx].occupants.append(entities.create_entity("Dummy", curx, cury, 3, "$", red, 0, 30))
-    
+            Map[cury][curx].occupants.append(entities.create_enemy("Dummy", curx, cury, 3, "$", red, 30))
     
     return Map
+
+
+def distance(ax, ay, bx, by):
+    xdist = ax - bx
+    ydist = ay - by
+    answer = math.sqrt((xdist ** 2) + (ydist ** 2))
+    return answer
