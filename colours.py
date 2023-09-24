4ab113009bc7e4e7067a76ecd2844a076615a6c8
Sludderr <108181849+Sludderr@users.noreply.github.com>
2023-09-24 18:32:05 +0100

    Update colours.py

--- colours.py
+++ colours.py
@@ -8,8 +8,24 @@ colours = {
     "camel": (193, 154, 107),
     "slate": (112, 128, 144),
     "darkslate": (47, 79, 79),
-    "red": (255,0,0),
-    "maroon": (100,14,39)
+    "red": (255, 0, 0),
+    "maroon": (100, 15, 40),
+    "lightblue": (173, 216, 230),
+    "gray": (128,128,128),
+    "sandybrown": (244,164,96),
+    "goldenrod": (218,165,32),
+    "rosybrown": (188,143,143),
+    "chocolate": (210,105,30),
+    "pink": (255,192,203),
+    "paleviolet":(219,112,149),
+    "salmon": (250, 128, 114),
+    "purple": (128,0,128),
+    "indigo": (75,0,130),
+    "mediumpurple": (147,112,219),
+    "darkaquamarine": (102,205,170),
+    "teal": (0,128,128),
+    "crimson": (220,20,60),
+    "olive": (128,128,0)
 }
 def getcolours():
     return colours
